c08a87f886e921b1aa531f1c871ed2e96a85defc
minhogang <minhogang@yahoo.com>
2019-12-02 00:11:30 +1000

    Add main menu

---
+++ core/src/com/breakout/MainMenu.java
@@ -0,0 +1,76 @@
+package com.breakout;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ *
+ * @author sraven0
+ */
+public class MainMenu implements Screen {
+
+    BitmapFont font;
+    SpriteBatch spriteBatch;
+    Breakout game;
+
+    MainMenu(Breakout game) {
+        this.game = game;
+        font = new BitmapFont();
+        spriteBatch = new SpriteBatch();
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to Breakout", 100, 150);
+        game.font.draw(game.batch, "A small game by Min Ho Gang and Karl Adriano", 120, 130);
+        game.font.draw(game.batch, "Press any key to begin.", 120, 110);
+        game.batch.end();
+
+        if (Gdx.input.justTouched() || Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            game.setScreen(new GameScreen());
+            dispose();
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            Gdx.app.exit();
+        }
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
--- desktop/src/com/breakout/desktop/DesktopLauncher.java
+++ desktop/src/com/breakout/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.breakout.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.breakout.Main;
+import com.breakout.Breakout;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+		new LwjglApplication(new Breakout(), config);
 	}
 }
